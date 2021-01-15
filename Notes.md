24142ed4e9db459589fde3243a8e214f55b53fe6
Harsh <harsh.make1998@gmail.com>
2021-01-15 18:12:52 +0530

    added some more topics

---
+++ Notes.md
@@ -0,0 +1,11 @@
+# Notes 
+
+### Interfaces 
+
+- General straregy for reusable code in TS is create function with arguments that are typed with interfaces.
+- Object/Classes can decide to implement a given interface to work with a function.
+  
+### Classes
+
+- Class is a blueprint to create an object with some fields and methods to represent a thing
+  
---
+++ interfaces.ts
@@ -0,0 +1,76 @@
+// example without interfaces
+const car = {
+    name: 'yaris',
+    brand: 'toyota',
+    broken: true,
+    year: 2004,
+    kms: 13432,
+    user: 'john doe'
+}
+
+const viewDetails = (vehicle: { name: string, brand: string, broken: boolean, year: number, kms: number, user: string }) => {
+    console.log(vehicle.name);
+    console.log(vehicle.user);
+    console.log(vehicle.kms);
+};
+
+viewDetails(car);
+
+
+// example with interfaces 
+interface Car {
+    name: string;
+    brand: string;
+    broken: boolean;
+    year: number;
+    kms: number;
+    user: string;
+}
+
+const viewDetailsNew = (car: Car) => {
+    console.log(car.brand);
+    console.log(car.user);
+    console.log(car.kms);
+}
+
+const cardetail: Car = {
+    name: 'yaris',
+    brand: 'toyota',
+    broken: true,
+    year: 2004,
+    kms: 13432,
+    user: 'john doe'
+}
+
+viewDetailsNew(cardetail)
+
+// importent use
+interface Reportable {
+    summary(): string;
+}
+
+const newCar = {
+    name: 'yaris',
+    brand: 'toyota',
+    broken: true,
+    summary(): string {
+        return `Summary: ${this.name} + ${this.brand} + is ${this.broken ? 'borken' : 'not broken'}`
+    }
+}
+
+const drink = {
+    color: 'black',
+    name: 'cola',
+    summary(): string {
+        return `Summary: ${this.name} + ${this.color}`
+    }
+}
+
+const printSummary = (item: Reportable) => {
+    console.log(item.summary())
+}
+
+printSummary(newCar);
+printSummary(drink);
+
+// conclusion : can use single interface for multiple function arguments. 
